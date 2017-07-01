a0d7071471819c15f903ee61025a3796e34edc1a
mohamedtwice <mohamedtwice@gmail.com>
2017-06-30 21:04:56 -0500

    added final styling

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var app =express();
-var path= require('path');
+var app = express();
+var path = require('path');
 
 
 app.use(express.static('public'));
 
-var port = 3000;
+var port = 3333;
 
-app.listen(port, function(){
+app.listen(port, function() {
   console.log('server up on: ', port);
 });
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(path.resolve('public/views/index.html'));
 });
